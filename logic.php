f739ef82e9b467fef0592de9d6b4948927184702
David Mares <maresdavide6704@outlook.com>
2022-02-25 13:26:38 -0700

    Add files via upload

--- logic.php
+++ logic.php
@@ -6,14 +6,21 @@
              * I used https://www.javatpoint.com/php-append-to-file for help in appending a line to the txt file
              * I did this because put contents function would delete all previous info of the txt file
              */
+
+            if(!isset($_POST['name']) || !isset($_POST['PLP'])){//checking if input is empty
+                $_POST['name'] = "";
+                $_POST['PLP'] = "";
+            }else{
             $writer = fopen('Items.txt','a');
             $input = $_POST['name'] . "," . $_POST['PLP'] . ",";
             fwrite($writer,$input);
             fclose($writer);
-
+            }
             $commonFile = file_get_contents('Common.txt');//reads file and returns a string
             $commonStr = explode(',', $commonFile);//seprates the string by ','
-            if(count($commonStr) != 0){// if common is not empty then print out arrayu
+            if(count($commonStr) != 0){// if common is not empty then print out array
+                echo "<h3> Common Items </h3>";
+                echo "<h5> Name  PLU</h5>";
                 for($i = 0; $i < count($commonStr)-1; $i+=2){
                     echo $commonStr[$i] . " " . $commonStr[$i+1] . "<br>";
                 }
@@ -26,9 +33,14 @@
             }
             //array_push($itemList,array($_POST['name'],$_POST['PLP']));
             sort($itemList);
+            echo "<h3> Items </h3>";
+            echo "<h5> Name  PLU</h5>";
             foreach($itemList as $row){
-                echo "<input type = checkbox value = Checked>";
+                if (!empty($row[0])){
+                
+                echo "<input type = checkbox name = Checked>";
                 echo  $row[0] . "\t\t" . $row[1] . "<br>";
+                }
             }
         ?>
         <input type = "submit" name = "moving" value = "Move to Top">
@@ -37,6 +49,7 @@
             <?php
             if(isset($_POST['moving'])){
                 $writer = fopen('Common.txt');
+                $temp = $_POST['Checked'];
             }
             ?>
     </body>
